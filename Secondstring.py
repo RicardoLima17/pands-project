f5e19e68ff1cda3ce785cb4c62b93fdc786b5c0e
RicardoLima17 <77755223+RicardoLima17@users.noreply.github.com>
2021-03-29 19:30:20 +0100

    Input a string and outputs every second letter in reverse order

---
+++ Secondstring.py
@@ -0,0 +1,11 @@
+# Author Ricardo
+
+# type  The quick brown fox jumps over the lazy dog.
+sentence = input("Type a sentence that you want reversed:")
+
+# reverse a sentence every second letter in reverse order.  [::-2] get 2 in 2 letters
+print(sentence[::-2])
+
+
+# References https://djangocentral.com/reverse-a-sentence/
+# Used lecture on Tuesday program as a base for the problem.
