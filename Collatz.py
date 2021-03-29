529bbba215d4f1cca7aa29a95cc5c37fbc2ee291
RicardoLima17 <77755223+RicardoLima17@users.noreply.github.com>
2021-03-29 19:46:52 +0100

    Input any positive integer and outputs the result

--- Collatz.py
+++ Collatz.py
@@ -1,11 +1,14 @@
 # Author Ricardo Rodrigues
-       
+  
+# Defining the function 
 def collatz(number):
 
+ # If the number is negative print "Unfortunately this is not a positive Integer."
  if number <= 0:
        print("Unfortunately this is not a positive Integer.") 
        quit() 
-
+       
+# looping through and if number found is even divide it by 2
  elif number % 2 == 0:
         print(number // 2)
         return number // 2
@@ -14,8 +17,11 @@ def collatz(number):
         result = 3 * number + 1
         print(result)
         return result
-
+       
+# Type the number
 n = input("Type a number: ")
+
+# running the function togethe final result number 1
 while n != 1:
     n = collatz(int(n))
 
